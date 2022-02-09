2b854ee3c95d82a474faf25822297cad0d8fa718
AlexSmetanin <AlexSmetanin72@gmail.com>
2022-02-09 23:41:33 +0200

    First commit

---
+++ src/test/java/ua/edu/chpt/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.edu.chpt.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
