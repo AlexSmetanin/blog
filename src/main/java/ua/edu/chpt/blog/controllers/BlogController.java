43693bff9b0a41cf23cde6725eb72aa3e38d1c7f
AlexSmetanin <AlexSmetanin72@gmail.com>
2022-02-17 00:46:33 +0200

    Urok#5 Finished

--- src/main/java/ua/edu/chpt/blog/controllers/BlogController.java
+++ src/main/java/ua/edu/chpt/blog/controllers/BlogController.java
@@ -1,9 +1,12 @@
 package ua.edu.chpt.blog.controllers;
 
+import org.hibernate.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ua.edu.chpt.blog.models.Post;
 import ua.edu.chpt.blog.repo.PostRepository;
 
@@ -20,4 +23,15 @@ public class BlogController {
         return "blog-main";
     }
 
+    @GetMapping("/blog/add")
+    public String blogAdd(Model model) {
+        return "blog-add";
+    }
+
+    @PostMapping("/blog/add")
+    public String blogPostAdd(@RequestParam String title, @RequestParam String anons, @RequestParam String full_text, Model model) {
+        Post post = new Post(title, anons, full_text);
+        postRepository.save(post);
+        return "redirect:/blog";
+    }
 }
